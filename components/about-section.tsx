cf487c20524fc1dca2769a7ee33e0c7f3b4b9d1d
DivyanshiGupta-18 <guptadivyanshi756@gmail.com>
2025-08-07 23:52:43 +0530

    num

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { motion } from "framer-motion"
-import { Download, Mail, MapPin, Phone } from "lucide-react"
+import { Download, Mail, MapPin } from "lucide-react"
 import Image from "next/image"
 
 export default function AboutSection() {
